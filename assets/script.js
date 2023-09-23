bb833c2c92112beffdab434f3d30047179e7129e
michael seaman <mseaman26@gmail.com>
2023-09-23 00:36:28 -0700

    #282 SUGAR

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "MONEY"
-let currentWordleNumber = 281 //<---------Change wordle number here
+let wordAsString = "SUGAR"
+let currentWordleNumber = 282 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
