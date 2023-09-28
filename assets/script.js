17e841e5707196836a2d7f3e7eff86ab6b60c321
michael seaman <mseaman26@gmail.com>
2023-09-28 12:02:53 -0700

    #286 TRAIT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ROWDY"
-let currentWordleNumber = 285 //<---------Change wordle number here
+let wordAsString = "TRAIT"
+let currentWordleNumber = 286 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
