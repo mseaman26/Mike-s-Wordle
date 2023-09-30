64ac9dc0aef309387c338df65e7118cdec6e4ff7
michael seaman <mseaman26@gmail.com>
2023-09-30 01:35:05 -0700

    #288 WEIGH

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SIRUP"
-let currentWordleNumber = 287 //<---------Change wordle number here
+let wordAsString = "WEIGH"
+let currentWordleNumber = 288 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
