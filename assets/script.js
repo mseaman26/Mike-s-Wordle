bbc6c6a6570f50d250773573b343270a904c824e
michael seaman <mseaman26@gmail.com>
2023-10-01 01:50:27 -0700

    #289 PINCH

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WEIGH"
-let currentWordleNumber = 288 //<---------Change wordle number here
+let wordAsString = "PINCH"
+let currentWordleNumber = 289 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
