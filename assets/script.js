574dca62bf28d69f665b1c4b75f89df91dfdbcee
michael seaman <mseaman26@gmail.com>
2023-10-03 00:54:02 -0700

    #290 BLINK

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PINCH"
-let currentWordleNumber = 289 //<---------Change wordle number here
+let wordAsString = "BLINK"
+let currentWordleNumber = 290 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
