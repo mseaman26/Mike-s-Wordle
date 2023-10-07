6b549c06be63426ff1bf50508267c7ce8dc95c05
michael seaman <mseaman26@gmail.com>
2023-10-07 01:16:13 -0700

    #294 CROWN

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CHOIR"
-let currentWordleNumber = 293 //<---------Change wordle number here
+let wordAsString = "CROWN"
+let currentWordleNumber = 294 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
