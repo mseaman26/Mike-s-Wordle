46774407fa263b855929ca664cb04f1f869e0820
michael seaman <mseaman26@gmail.com>
2023-10-08 00:50:31 -0700

    #295 JUDGE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CROWN"
-let currentWordleNumber = 294 //<---------Change wordle number here
+let wordAsString = "JUDGE"
+let currentWordleNumber = 295 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
