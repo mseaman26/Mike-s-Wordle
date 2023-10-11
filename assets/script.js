953fb3fda2d47efc603d8296ac64d8231132346b
michael seaman <mseaman26@gmail.com>
2023-10-11 01:47:23 -0700

    #298 WAGER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ONION"
-let currentWordleNumber = 297 //<---------Change wordle number here
+let wordAsString = "WAGER"
+let currentWordleNumber = 298 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
