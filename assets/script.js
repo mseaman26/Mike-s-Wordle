e59260fa6095cfff5673070117e33a0d54dabbed
michael seaman <mseaman26@gmail.com>
2023-10-15 00:26:16 -0700

    #302 BUILD

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LARGE"
-let currentWordleNumber = 301 //<---------Change wordle number here
+let wordAsString = "BUILD"
+let currentWordleNumber = 302 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
