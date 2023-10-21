aa4923584aec2f8ff72cc79c4fb0046c7e77ee48
michael seaman <mseaman26@gmail.com>
2023-10-21 01:50:58 -0700

    #308 EJECT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "MEDIA"
-let currentWordleNumber = 307 //<---------Change wordle number here
+let wordAsString = "EJECT"
+let currentWordleNumber = 308 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
