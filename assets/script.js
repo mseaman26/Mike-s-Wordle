060ba9c49bbe77647cc0709a77b203de544df93a
michael seaman <mseaman26@gmail.com>
2023-10-22 01:52:17 -0700

    #309 SYRUP

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "EJECT"
-let currentWordleNumber = 308 //<---------Change wordle number here
+let wordAsString = "SYRUP"
+let currentWordleNumber = 309 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
