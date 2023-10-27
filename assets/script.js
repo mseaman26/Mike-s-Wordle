8a617cc757b9dbee968e90bd25d06380b72947dd
michael seaman <mseaman26@gmail.com>
2023-10-27 01:23:50 -0700

    #314 SIREN

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WRITE"
-let currentWordleNumber = 313 //<---------Change wordle number here
+let wordAsString = "SIREN"
+let currentWordleNumber = 314 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
