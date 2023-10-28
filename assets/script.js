5745d6f80f4ff79e9735d60dbc9146f49c0010d6
michael seaman <mseaman26@gmail.com>
2023-10-28 01:20:58 -0700

    #315 CRAWL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SIREN"
-let currentWordleNumber = 314 //<---------Change wordle number here
+let wordAsString = "CRAWL"
+let currentWordleNumber = 315 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
