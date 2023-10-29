b93b388bdc095b9937fbc38981a89a3fda54d510
michael seaman <mseaman26@gmail.com>
2023-10-29 01:25:15 -0700

    #316 PULSE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CRAWL"
-let currentWordleNumber = 315 //<---------Change wordle number here
+let wordAsString = "PULSE"
+let currentWordleNumber = 316 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
