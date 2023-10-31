a9caaf8b9fb5ff6430f6e9480691fc24c81c9412
michael seaman <mseaman26@gmail.com>
2023-10-30 19:59:00 -0700

    #317 IDIOT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PULSE"
-let currentWordleNumber = 316 //<---------Change wordle number here
+let wordAsString = "IDIOT"
+let currentWordleNumber = 317 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
