311bf3e1f7d7e65b0e97334092214840d14de318
michael seaman <mseaman26@gmail.com>
2023-10-31 00:50:27 -0700

    #318 FROST

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "IDIOT"
-let currentWordleNumber = 317 //<---------Change wordle number here
+let wordAsString = "FROST"
+let currentWordleNumber = 318 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
