7e3b12df6a29370a652c2b944376fcb8ddc97794
michael seaman <mseaman26@gmail.com>
2023-11-02 01:40:52 -0700

    #319 UNION

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FROST"
-let currentWordleNumber = 318 //<---------Change wordle number here
+let wordAsString = "UNION"
+let currentWordleNumber = 319 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
