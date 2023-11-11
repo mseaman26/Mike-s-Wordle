1c7b6087ef3481ce5295cd5e3a4ec1c72fb134e7
michael seaman <mseaman26@gmail.com>
2023-11-11 00:07:39 -0800

    #328

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "BUDDY"
-let currentWordleNumber = 327 //<---------Change wordle number here
+let wordAsString = "SNOOP"
+let currentWordleNumber = 328 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
