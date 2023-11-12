e94130fa618af025e18ce69d940db48e9c8b11ae
michael seaman <mseaman26@gmail.com>
2023-11-12 01:34:21 -0800

    #329 TONIC

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SNOOP"
-let currentWordleNumber = 328 //<---------Change wordle number here
+let wordAsString = "TONIC"
+let currentWordleNumber = 329 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
