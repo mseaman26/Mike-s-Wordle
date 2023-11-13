7fadf6cc00b3549cb73603fc3efb3f2d437ad98a
michael seaman <mseaman26@gmail.com>
2023-11-13 01:14:04 -0800

    #330 BLURT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "TONIC"
-let currentWordleNumber = 329 //<---------Change wordle number here
+let wordAsString = "BLURT"
+let currentWordleNumber = 330 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
