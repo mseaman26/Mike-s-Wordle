e788399633fdc309d814e6c5abc9bf8bd386c303
michael seaman <mseaman26@gmail.com>
2023-11-16 19:53:28 -0800

    #333 CIVIL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PARTY"
-let currentWordleNumber = 332 //<---------Change wordle number here
+let wordAsString = "CIVIL"
+let currentWordleNumber = 333 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
