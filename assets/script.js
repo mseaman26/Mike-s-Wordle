baefe2a9eaf00e2e36afcd5bd7026277cc77dab6
michael seaman <mseaman26@gmail.com>
2023-11-19 01:46:03 -0800

    #336 SQUAD

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SEGUE"
-let currentWordleNumber = 335 //<---------Change wordle number here
+let wordAsString = "SQUAD"
+let currentWordleNumber = 336 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
