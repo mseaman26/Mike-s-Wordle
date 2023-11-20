af1b6b2fd251b18991735e542c27149d5d5f0187
michael seaman <mseaman26@gmail.com>
2023-11-20 00:03:19 -0800

    #337 VALVE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SQUAD"
-let currentWordleNumber = 336 //<---------Change wordle number here
+let wordAsString = "VALVE"
+let currentWordleNumber = 337 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
