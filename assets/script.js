3c38bc42e2d1e326e92459c23c10a952cbfbb812
michael seaman <mseaman26@gmail.com>
2023-11-21 01:17:03 -0800

    #338

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "VALVE"
-let currentWordleNumber = 337 //<---------Change wordle number here
+let wordAsString = "REVEL"
+let currentWordleNumber = 338 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
