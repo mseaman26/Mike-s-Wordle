ea7461d630ce850cd543f0babb0d0859f6f8ff32
michael seaman <mseaman26@gmail.com>
2023-12-07 00:23:47 -0800

    #353 ARROW

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ERECT"
-let currentWordleNumber = 352 //<---------Change wordle number here
+let wordAsString = "ARROW"
+let currentWordleNumber = 353 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
