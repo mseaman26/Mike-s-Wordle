ca379d4f551ff3435b3d968e929fbf093727d32d
michael seaman <mseaman26@gmail.com>
2023-12-09 16:18:38 -0800

    #355 ORDER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SMITE"
-let currentWordleNumber = 354 //<---------Change wordle number here
+let wordAsString = "ORDER"
+let currentWordleNumber = 355 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
