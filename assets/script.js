6069b231281730a0fc6bc5508bff33085e081b96
michael seaman <mseaman26@gmail.com>
2023-12-10 18:08:10 -0800

    #356 LAYER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ORDER"
-let currentWordleNumber = 355 //<---------Change wordle number here
+let wordAsString = "LAYER"
+let currentWordleNumber = 356 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
