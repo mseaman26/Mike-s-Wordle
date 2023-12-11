ced0fc42a61589da51a4226d0ca0ba630351d9c3
michael seaman <mseaman26@gmail.com>
2023-12-11 01:45:15 -0800

    #357 SLUMP'

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LAYER"
-let currentWordleNumber = 356 //<---------Change wordle number here
+let wordAsString = "SLUMP"
+let currentWordleNumber = 357 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
