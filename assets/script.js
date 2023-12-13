654b32cef64498ac3f0219941bf31a57f5fd2ba1
michael seaman <mseaman26@gmail.com>
2023-12-13 00:26:19 -0800

    #359 OCTET

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SLUMP"
-let currentWordleNumber = 358 //<---------Change wordle number here
+let wordAsString = "OCTET"
+let currentWordleNumber = 359 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
