390c34636f04877e3bc97d3385ef127aecbf0aee
michael seaman <mseaman26@gmail.com>
2023-12-16 01:04:07 -0800

    #360 FINAL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "OCTET"
-let currentWordleNumber = 359 //<---------Change wordle number here
+let wordAsString = "FINAL"
+let currentWordleNumber = 360 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
