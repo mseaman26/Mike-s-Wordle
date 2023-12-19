8e8818dbad10386a1a1c9ee88bb4c690be28f367
michael seaman <mseaman26@gmail.com>
2023-12-19 10:55:37 -0800

    #365 YEARN

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "DICEY"
-let currentWordleNumber = 364 //<---------Change wordle number here
+let wordAsString = "YEARN"
+let currentWordleNumber = 365 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
