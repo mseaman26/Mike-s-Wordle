683ac61de5b879c28799c3d4421cdf36b41314cd
michael seaman <mseaman26@gmail.com>
2023-12-21 01:19:13 -0800

    #367 FIEND

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "AWASH"
-let currentWordleNumber = 366 //<---------Change wordle number here
+let wordAsString = "FIEND"
+let currentWordleNumber = 367 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
