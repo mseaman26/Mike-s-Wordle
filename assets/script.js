c36704f7391355c950c2448085bd4308f3d66937
michael seaman <mseaman26@gmail.com>
2023-12-27 00:23:17 -0800

    #373 RETRO

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FRAUD"
-let currentWordleNumber = 372 //<---------Change wordle number here
+let wordAsString = "RETRO"
+let currentWordleNumber = 373 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
