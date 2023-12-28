8a91ca726ffa050590c2f14af5f9225f0be23820
michael seaman <mseaman26@gmail.com>
2023-12-28 01:06:36 -0800

    #374 NINJA

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "RETRO"
-let currentWordleNumber = 373 //<---------Change wordle number here
+let wordAsString = "NINJA"
+let currentWordleNumber = 374 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
