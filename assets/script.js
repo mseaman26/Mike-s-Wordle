14355eba89c83ce592cbc374dc523e47de350c7b
michael seaman <mseaman26@gmail.com>
2023-12-29 01:43:16 -0800

    #375 WORTH

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "NINJA"
-let currentWordleNumber = 374 //<---------Change wordle number here
+let wordAsString = "WORTH"
+let currentWordleNumber = 375 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
