670a45586acd12ff1e43316429e9f83bf6cb5860
michael seaman <mseaman26@gmail.com>
2023-12-30 01:02:46 -0800

    #376 AROMA

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WORTH"
-let currentWordleNumber = 375 //<---------Change wordle number here
+let wordAsString = "AROMA"
+let currentWordleNumber = 376 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
