cac905b9d873bb75cdf52ad4f2e5d7941a1f917d
michael seaman <mseaman26@gmail.com>
2023-12-31 11:53:30 -0800

    #377 ENSUE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "AROMA"
-let currentWordleNumber = 376 //<---------Change wordle number here
+let wordAsString = "ENSUE"
+let currentWordleNumber = 377 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
