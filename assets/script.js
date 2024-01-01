be5c3f62243c360d778a0173e8bae82bc8fa71fd
michael seaman <mseaman26@gmail.com>
2024-01-01 02:03:37 -0800

    #378 FLASK

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ENSUE"
-let currentWordleNumber = 377 //<---------Change wordle number here
+let wordAsString = "FLASK"
+let currentWordleNumber = 378 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
