fdbdde63ba9652cacfc1fad520cf7fc1f17b6396
michael seaman <mseaman26@gmail.com>
2024-01-02 00:47:08 -0800

    #379 WEDGE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FLASK"
-let currentWordleNumber = 378 //<---------Change wordle number here
+let wordAsString = "WEDGE"
+let currentWordleNumber = 379 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
