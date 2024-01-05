2a3408c2194befc642e69429fb7f8beef259758e
michael seaman <mseaman26@gmail.com>
2024-01-04 20:07:40 -0800

    #380 WALTZ

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WEDGE"
-let currentWordleNumber = 379 //<---------Change wordle number here
+let wordAsString = "WALTZ"
+let currentWordleNumber = 380 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
