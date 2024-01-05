647b52fbcac853702c067bbb394156f3cc50fc51
michael seaman <mseaman26@gmail.com>
2024-01-05 12:37:16 -0800

    #381 ALONE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WALTZ"
-let currentWordleNumber = 380 //<---------Change wordle number here
+let wordAsString = "ALONE"
+let currentWordleNumber = 381 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
