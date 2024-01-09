9dafbbf48618ab1980623c85ce6b59712eda5ba5
michael seaman <mseaman26@gmail.com>
2024-01-08 19:42:37 -0800

    #384 WASTE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "VALID"
-let currentWordleNumber = 383 //<---------Change wordle number here
+let wordAsString = "WASTE"
+let currentWordleNumber = 384 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
