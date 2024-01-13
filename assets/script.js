1b2f4a818a4eef8a5ec46ed2c52c52543d234639
michael seaman <mseaman26@gmail.com>
2024-01-13 01:54:20 -0800

    #388 ENTRY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "MIDST"
-let currentWordleNumber = 387 //<---------Change wordle number here
+let wordAsString = "ENTRY"
+let currentWordleNumber = 388 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
