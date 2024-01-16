cc500362ea94bcf27fb770bdc7b643a4a4a131a8
michael seaman <mseaman26@gmail.com>
2024-01-16 01:03:59 -0800

    #391 DECOY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "JELLY"
-let currentWordleNumber = 390 //<---------Change wordle number here
+let wordAsString = "DECOY"
+let currentWordleNumber = 391 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
