914ad23e58f78d6fdb7cf8be9b862dc0eb7722ee
michael seaman <mseaman26@gmail.com>
2024-01-23 15:22:46 -0800

    #398 GLORY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SCOOP"
-let currentWordleNumber = 397 //<---------Change wordle number here
+let wordAsString = "GLORY"
+let currentWordleNumber = 398 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
