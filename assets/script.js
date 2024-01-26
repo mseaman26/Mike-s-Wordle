8dbd3b9502103c92dc52d19b6bf3c76b90662287
michael seaman <mseaman26@gmail.com>
2024-01-26 11:55:45 -0800

    #400 UNITE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "STOOL"
-let currentWordleNumber = 399 //<---------Change wordle number here
+let wordAsString = "UNITE"
+let currentWordleNumber = 400 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
