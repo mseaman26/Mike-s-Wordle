14a3b108d6b98b4c01fda8fe94207eb47276042a
michael seaman <mseaman26@gmail.com>
2024-01-27 11:15:52 -0800

    #402 BLURB

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "EERIE"
-let currentWordleNumber = 401 //<---------Change wordle number here
+let wordAsString = "BLURB"
+let currentWordleNumber = 402 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
