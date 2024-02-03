0154ecc7dadf51d249b29248fff839374b64b35e
michael seaman <mseaman26@gmail.com>
2024-02-03 01:29:12 -0800

    #407 ALOUD

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "TOOTH"
-let currentWordleNumber = 406 //<---------Change wordle number here
+let wordAsString = "ALOUD"
+let currentWordleNumber = 407 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
