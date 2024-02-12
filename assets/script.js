7c8dcc2700262e98bdafc78a824c863339f6496f
michael seaman <mseaman26@gmail.com>
2024-02-12 12:15:20 -0800

    #414 IRATE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "GROAN"
-let currentWordleNumber = 413 //<---------Change wordle number here
+let wordAsString = "IRATE"
+let currentWordleNumber = 414 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
