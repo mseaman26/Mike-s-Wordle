65f9459d0eca46e8d98c295a176cbe83ea2a0a43
michael seaman <mseaman26@gmail.com>
2024-02-17 00:16:21 -0800

    #417 DELAY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FROTH"
-let currentWordleNumber = 416 //<---------Change wordle number here
+let wordAsString = "DELAY"
+let currentWordleNumber = 417 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
