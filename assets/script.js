66dc2c893110a1235d6048d5d052ae31025eb7c6
michael seaman <mseaman26@gmail.com>
2024-02-20 17:44:16 -0800

    #419 EXUDE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CHIRP"
-let currentWordleNumber = 418 //<---------Change wordle number here
+let wordAsString = "EXUDE"
+let currentWordleNumber = 419 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
