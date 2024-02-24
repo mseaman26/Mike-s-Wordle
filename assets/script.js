12e1418ea548627b1c7bfae8ccd9b98073ee6ac9
michael seaman <mseaman26@gmail.com>
2024-02-24 13:59:54 -0800

    #423 XEROX

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SCALP"
-let currentWordleNumber = 422 //<---------Change wordle number here
+let wordAsString = "XEROX"
+let currentWordleNumber = 423 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
