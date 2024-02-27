9e99cf3dd91e123ef94ce9a5e52f26d961e876ef
michael seaman <mseaman26@gmail.com>
2024-02-27 13:10:53 -0800

    #425 FEIGN

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "VINYL"
-let currentWordleNumber = 424 //<---------Change wordle number here
+let wordAsString = "FEIGN"
+let currentWordleNumber = 425 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
