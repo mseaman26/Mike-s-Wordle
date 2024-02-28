0e91e0e12e77e08c8ef96e46a633b157f1cd1d3d
michael seaman <mseaman26@gmail.com>
2024-02-28 13:53:51 -0800

    #426 LAUCH

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FEIGN"
-let currentWordleNumber = 425 //<---------Change wordle number here
+let wordAsString = "LAUCH"
+let currentWordleNumber = 426 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
