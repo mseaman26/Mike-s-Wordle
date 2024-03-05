014a4299d22fd41b2d17db18c2a9ab86d18c7c06
michael seaman <mseaman26@gmail.com>
2024-03-05 01:22:36 -0800

    #430 USHER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "STRIP"
-let currentWordleNumber = 429 //<---------Change wordle number here
+let wordAsString = "USHER"
+let currentWordleNumber = 430 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
