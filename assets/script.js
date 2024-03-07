40933559998a5a0f707247d7788015b996d3a161
michael seaman <mseaman26@gmail.com>
2024-03-07 01:36:52 -0800

    32 JUICE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "MODEM"
-let currentWordleNumber = 431 //<---------Change wordle number here
+let wordAsString = "JUICE"
+let currentWordleNumber = 432 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
