45f452daf5d4c28e5dd36fd44c0bd852a0c02312
michael seaman <mseaman26@gmail.com>
2024-03-10 00:54:11 -0800

    #434 ASKEW

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ALLOW"
-let currentWordleNumber = 433 //<---------Change wordle number here
+let wordAsString = "ASKEW"
+let currentWordleNumber = 434 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
