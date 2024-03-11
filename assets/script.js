45a5eef5332f823b701b743df05c1b98c3a8313c
michael seaman <mseaman26@gmail.com>
2024-03-11 10:41:08 -0700

    #435 AUDIT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ASKEW"
-let currentWordleNumber = 434 //<---------Change wordle number here
+let wordAsString = "AUDIT"
+let currentWordleNumber = 435 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
