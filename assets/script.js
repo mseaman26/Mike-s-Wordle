3e8f7df50dc32dc4d57c8bd6343350c17be62944
michael seaman <mseaman26@gmail.com>
2024-03-16 02:06:10 -0700

    #438 USURP

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "KAYAK"
-let currentWordleNumber = 437 //<---------Change wordle number here
+let wordAsString = "USURP"
+let currentWordleNumber = 438 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
