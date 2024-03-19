4065446cb705a424505ff72eceab6c17a0e2717a
michael seaman <mseaman26@gmail.com>
2024-03-19 12:42:31 -0700

    #439 PIXIE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "USURP"
-let currentWordleNumber = 438 //<---------Change wordle number here
+let wordAsString = "PIXIE"
+let currentWordleNumber = 439 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
