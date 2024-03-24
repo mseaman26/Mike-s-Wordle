80c741b136039287083074c4797da9d34449d880
michael seaman <mseaman26@gmail.com>
2024-03-24 00:31:32 -0700

    43 KNOCK

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ALIGN"
-let currentWordleNumber = 442 //<---------Change wordle number here
+let wordAsString = "KNOCK"
+let currentWordleNumber = 443 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
