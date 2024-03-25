cafa1c07310dd1d01c405976304d74f8582c6cd2
michael seaman <mseaman26@gmail.com>
2024-03-25 00:37:40 -0700

    #444 SHOVE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "KNOCK"
-let currentWordleNumber = 443 //<---------Change wordle number here
+let wordAsString = "SHOVE"
+let currentWordleNumber = 444 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
