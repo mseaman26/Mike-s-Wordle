863defc0ab4e86f7544288967b1268ae53461c58
michael seaman <mseaman26@gmail.com>
2024-04-01 00:42:48 -0700

    #451 IGLOO

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "STUNT"
-let currentWordleNumber = 450 //<---------Change wordle number here
+let wordAsString = "IGLOO"
+let currentWordleNumber = 451 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
