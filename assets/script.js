1e5a09549f73bdd54730d87bee64c9ca3c47c06c
michael seaman <mseaman26@gmail.com>
2024-04-03 10:49:35 -0700

    #453 ARGUE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SALAD"
-let currentWordleNumber = 452 //<---------Change wordle number here
+let wordAsString = "ARGUE"
+let currentWordleNumber = 453 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
