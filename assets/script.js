7f34a6d887926702500a53044617c26f66300265
michael seaman <mseaman26@gmail.com>
2024-04-05 10:56:24 -0700

    #455 BURST

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FUCUS"
-let currentWordleNumber = 454 //<---------Change wordle number here
+let wordAsString = "BURST"
+let currentWordleNumber = 455 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
