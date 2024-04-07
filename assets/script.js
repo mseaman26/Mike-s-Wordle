fde96305aecfabb19a7a1c32c75083f717886c14
michael seaman <mseaman26@gmail.com>
2024-04-06 19:24:30 -0700

    #456 KNEEL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "BURST"
-let currentWordleNumber = 455 //<---------Change wordle number here
+let wordAsString = "KNEEL"
+let currentWordleNumber = 456 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
