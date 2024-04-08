39fdd6dcd6f4ebfe08a31868ca3bc1f4ef8ce794
michael seaman <mseaman26@gmail.com>
2024-04-08 00:45:41 -0700

    #457 SKULL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "KNEEL"
-let currentWordleNumber = 456 //<---------Change wordle number here
+let wordAsString = "SKULL"
+let currentWordleNumber = 457 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
