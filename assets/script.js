66740fed87a9527ac17422f31faa0d02e5b7096e
michael seaman <mseaman26@gmail.com>
2024-04-10 00:19:05 -0700

    59 SWELL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "DIGIT"
-let currentWordleNumber = 458 //<---------Change wordle number here
+let wordAsString = "SWELL"
+let currentWordleNumber = 459 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
