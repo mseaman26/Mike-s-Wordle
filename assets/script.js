7749f0e6d7288c66c18dfab77c0ae423de51dd23
michael seaman <mseaman26@gmail.com>
2024-04-13 00:50:31 -0700

    60 SKIRT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SWELL"
-let currentWordleNumber = 459 //<---------Change wordle number here
+let wordAsString = "SKIRT"
+let currentWordleNumber = 460 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
