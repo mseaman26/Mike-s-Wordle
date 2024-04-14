35997671ab8f1184b4684f93e03da59db77f7efa
michael seaman <mseaman26@gmail.com>
2024-04-14 01:01:31 -0700

    61 SHRUG

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SKIRT"
-let currentWordleNumber = 460 //<---------Change wordle number here
+let wordAsString = "SHRUG"
+let currentWordleNumber = 461 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
