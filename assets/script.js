d8b5bff84e38f6c220afc4500481f3e9906b68c3
michael seaman <mseaman26@gmail.com>
2024-04-16 01:36:52 -0700

    #462

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SHRUG"
-let currentWordleNumber = 461 //<---------Change wordle number here
+let wordAsString = "LIVER"
+let currentWordleNumber = 462 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
