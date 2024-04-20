ba001c91de702c945bb027b89ec4ae039bb47484
michael seaman <mseaman26@gmail.com>
2024-04-20 10:15:14 -0700

    #465 CABLE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ENVOY"
-let currentWordleNumber = 464 //<---------Change wordle number here
+let wordAsString = "CABLE"
+let currentWordleNumber = 465 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
