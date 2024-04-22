9f5ddbe68605d3ef740d37e82dbf5ec13ea2e7fa
michael seaman <mseaman26@gmail.com>
2024-04-22 16:19:21 -0700

    #466 THIGH

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CABLE"
-let currentWordleNumber = 465 //<---------Change wordle number here
+let wordAsString = "THIGH"
+let currentWordleNumber = 466 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
