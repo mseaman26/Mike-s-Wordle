f9fd0a17734f44e25905a15a2fb1043b5951c876
michael seaman <mseaman26@gmail.com>
2024-04-24 00:57:11 -0700

    #467 EMOTE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "THIGH"
-let currentWordleNumber = 466 //<---------Change wordle number here
+let wordAsString = "EMOTE"
+let currentWordleNumber = 467 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
