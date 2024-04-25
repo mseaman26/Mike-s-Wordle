721e892bc1bf8d209814953133614b09dbcc978e
michael seaman <mseaman26@gmail.com>
2024-04-25 09:45:19 -0700

    68 SMEAR

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "EMOTE"
-let currentWordleNumber = 467 //<---------Change wordle number here
+let wordAsString = "SMEAR"
+let currentWordleNumber = 468 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
