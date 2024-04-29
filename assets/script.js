b39bebd40c9dcf8fccea3d8bc81effaf6aeb85b6
michael seaman <mseaman26@gmail.com>
2024-04-29 00:31:57 -0700

    #470 HAPPY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "GRAZE"
-let currentWordleNumber = 469 //<---------Change wordle number here
+let wordAsString = "HAPPY"
+let currentWordleNumber = 470 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
