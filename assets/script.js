1855a864730c0ab7f4cfc35d41340500cc818746
michael seaman <mseaman26@gmail.com>
2024-04-30 01:54:17 -0700

    #471 WRATH

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "HAPPY"
-let currentWordleNumber = 470 //<---------Change wordle number here
+let wordAsString = "WRATH"
+let currentWordleNumber = 471 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
