a892f0ce3dc166931baf7fb2687743f5e94fa403
michael seaman <mseaman26@gmail.com>
2024-05-07 02:16:54 -0700

    #478 EVENT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SYNTH"
-let currentWordleNumber = 477 //<---------Change wordle number here
+let wordAsString = "EVENT"
+let currentWordleNumber = 478 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
