59fcfc4ba57f0d007eb36a946cb09f9570ce8591
michael seaman <mseaman26@gmail.com>
2024-05-08 01:30:06 -0700

    #479 UNFIT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "EVENT"
-let currentWordleNumber = 478 //<---------Change wordle number here
+let wordAsString = "UNFIT"
+let currentWordleNumber = 479 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
