e1b3f5e1294ae3e4330c27f529a4d6337f01f6b1
michael seaman <mseaman26@gmail.com>
2024-05-09 01:00:01 -0700

    #480 BLEAK

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "UNFIT"
-let currentWordleNumber = 479 //<---------Change wordle number here
+let wordAsString = "BLEAK"
+let currentWordleNumber = 480 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
