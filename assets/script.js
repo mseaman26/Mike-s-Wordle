65cc67a7d8828a3c5ce55f47c03b1ef8a9a84c7d
michael seaman <mseaman26@gmail.com>
2024-05-10 01:58:56 -0700

    #481 SOLVE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "BLEAK"
-let currentWordleNumber = 480 //<---------Change wordle number here
+let wordAsString = "SOLVE"
+let currentWordleNumber = 481 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
