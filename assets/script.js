a6fe2c887cb446737c63f9b631de177961f8bf85
michael seaman <mseaman26@gmail.com>
2024-05-11 00:04:20 -0700

    #482 PANIC

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SOLVE"
-let currentWordleNumber = 481 //<---------Change wordle number here
+let wordAsString = "PANIC"
+let currentWordleNumber = 482 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
