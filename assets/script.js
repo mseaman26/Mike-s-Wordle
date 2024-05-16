593029381c9de8c671bbe0ff47242b01a8ef97b1
michael seaman <mseaman26@gmail.com>
2024-05-16 01:41:31 -0700

    #484 WITTY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "TAUNT"
-let currentWordleNumber = 483 //<---------Change wordle number here!
+let wordAsString = "WITTY"
+let currentWordleNumber = 484 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
