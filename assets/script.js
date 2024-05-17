faea6391e60fb7a98daac0aa676a4f757bc8c47a
michael seaman <mseaman26@gmail.com>
2024-05-17 01:17:09 -0700

    #485 SEMEN

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WITTY"
-let currentWordleNumber = 484 //<---------Change wordle number here!
+let wordAsString = "SEMEN"
+let currentWordleNumber = 485 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
