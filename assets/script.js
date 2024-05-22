85b55f737210b61a3413e6288cf67cc4bb63fee6
michael seaman <mseaman26@gmail.com>
2024-05-22 01:00:44 -0700

    #489 CORAL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "BOOZE"
-let currentWordleNumber = 488 //<---------Change wordle number here!
+let wordAsString = "CORAL"
+let currentWordleNumber = 489 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
