fbe61f48f14fa92d109b585f1d817331524fc424
michael seaman <mseaman26@gmail.com>
2024-05-30 17:37:42 -0700

    #496 CHURN

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "OLIVE"
-let currentWordleNumber = 495 //<---------Change wordle number here!
+let wordAsString = "CHURN"
+let currentWordleNumber = 496 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
