3bf2bc8982c65b7a1c7dec6c782627e691090fc7
michael seaman <mseaman26@gmail.com>
2024-06-01 00:55:59 -0700

    #497 PENIS

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CHURN"
-let currentWordleNumber = 496 //<---------Change wordle number here!
+let wordAsString = "PENIS"
+let currentWordleNumber = 497 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
