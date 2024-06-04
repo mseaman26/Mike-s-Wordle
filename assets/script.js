aff458b3a2715a4a3a0d6aaff3dd83999cab0149
michael seaman <mseaman26@gmail.com>
2024-06-04 01:33:30 -0700

    #500 ANNEX

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "GIANT"
-let currentWordleNumber = 499 //<---------Change wordle number here!
+let wordAsString = "ANNEX"
+let currentWordleNumber = 500 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
