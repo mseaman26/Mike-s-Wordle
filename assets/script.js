daf2e41dc868071bb6d6975a49b87bc1d1067482
michael seaman <mseaman26@gmail.com>
2024-06-09 02:46:35 -0700

    #504 FURRY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "IRONY"
-let currentWordleNumber = 503 //<---------Change wordle number here!
+let wordAsString = "FURRY"
+let currentWordleNumber = 504 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
