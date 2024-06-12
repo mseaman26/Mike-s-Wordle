2fbada472bab4234b985fb5af7bcc33b53d6bb46
michael seaman <mseaman26@gmail.com>
2024-06-12 00:36:53 -0700

    #506 PLUME

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FLUNK"
-let currentWordleNumber = 505 //<---------Change wordle number here!
+let wordAsString = "PLUME"
+let currentWordleNumber = 506 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
