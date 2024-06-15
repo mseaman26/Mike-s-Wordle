69fe813b28da3e41050c9e18ba0687ef0bcbf611
michael seaman <mseaman26@gmail.com>
2024-06-15 00:36:01 -0700

    #508 SLIME

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "COURT"
-let currentWordleNumber = 507 //<---------Change wordle number here!
+let wordAsString = "SLIME"
+let currentWordleNumber = 508 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
