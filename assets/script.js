d275c246392a91a2a77bba50d10e72f9592b999f
michael seaman <mseaman26@gmail.com>
2024-06-16 00:59:26 -0700

    #509 DEITY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SLIME"
-let currentWordleNumber = 508 //<---------Change wordle number here!
+let wordAsString = "DEITY"
+let currentWordleNumber = 509 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
