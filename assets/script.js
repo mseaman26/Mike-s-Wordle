c337c0707b81bbf084d592bdd3ba95c56f372fd1
michael seaman <mseaman26@gmail.com>
2024-06-20 01:28:38 -0700

    #512 DEUCE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PLEAD"
-let currentWordleNumber = 511 //<---------Change wordle number here!
+let wordAsString = "DEUCE"
+let currentWordleNumber = 512 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
