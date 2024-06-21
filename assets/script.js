8ff397be7088d995314f5e41fec6316ae9866799
michael seaman <mseaman26@gmail.com>
2024-06-21 01:07:26 -0700

    #513 EVADE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "DEUCE"
-let currentWordleNumber = 512 //<---------Change wordle number here!
+let wordAsString = "EVADE"
+let currentWordleNumber = 513 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
