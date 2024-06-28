7963e0a054c919a7f64dd24d51c8de5e8d6b891c
michael seaman <mseaman26@gmail.com>
2024-06-28 01:04:05 -0700

    #520 DOGMA

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "GRIEF"
-let currentWordleNumber = 519 //<---------Change wordle number here!
+let wordAsString = "DOGMA"
+let currentWordleNumber = 520 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
