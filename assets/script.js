1f64da3d91b75398aca2113fc018aff33e4e30d5
michael seaman <mseaman26@gmail.com>
2024-07-01 00:19:05 -0700

    #522 BLOND

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WAGON"
-let currentWordleNumber = 521 //<---------Change wordle number here!
+let wordAsString = "BLOND"
+let currentWordleNumber = 522 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
