d516913cf18d1af33208f985f80ffd3371c83b8d
michael seaman <mseaman26@gmail.com>
2024-07-02 01:21:41 -0700

    #523 RIDGE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "BLOND"
-let currentWordleNumber = 522 //<---------Change wordle number here!
+let wordAsString = "RIDGE"
+let currentWordleNumber = 523 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
