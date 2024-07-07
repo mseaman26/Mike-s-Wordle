eed8f2a9e85641c278d914b171fabf58766147e9
michael seaman <mseaman26@gmail.com>
2024-07-07 11:20:09 -0700

    #527 BANJO

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LEVEL"
-let currentWordleNumber = 526 //<---------Change wordle number here!
+let wordAsString = "BANJO"
+let currentWordleNumber = 527 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
