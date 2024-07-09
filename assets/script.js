2e4995ca76df35e2997191aa1d5290048697310a
michael seaman <mseaman26@gmail.com>
2024-07-09 01:51:02 -0700

    #528 FAULT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "BANJO"
-let currentWordleNumber = 527 //<---------Change wordle number here!
+let wordAsString = "FAULT"
+let currentWordleNumber = 528 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
