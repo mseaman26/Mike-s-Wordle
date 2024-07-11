e1d94ddf3c8f702c749cd002af1c3e8a52a23769
michael seaman <mseaman26@gmail.com>
2024-07-11 00:09:22 -0700

    #529 HORSE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FAULT"
-let currentWordleNumber = 528 //<---------Change wordle number here!
+let wordAsString = "HORSE"
+let currentWordleNumber = 529 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
