bbd9773af576aaa93d391b0142d6032ea2f61ec3
michael seaman <mseaman26@gmail.com>
2024-07-14 00:12:14 -0700

    #532 DUNCE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PEACH"
-let currentWordleNumber = 531 //<---------Change wordle number here!
+let wordAsString = "DUNCE"
+let currentWordleNumber = 532 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
