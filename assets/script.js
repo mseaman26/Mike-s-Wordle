315c3dd574469d97529dfaf7e70db6fdeecd1ee7
michael seaman <mseaman26@gmail.com>
2024-07-15 15:38:37 -0700

    #533 MAYOR

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "DUNCE"
-let currentWordleNumber = 532 //<---------Change wordle number here!
+let wordAsString = "MAYOR"
+let currentWordleNumber = 533 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
