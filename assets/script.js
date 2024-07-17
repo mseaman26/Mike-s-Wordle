a6c0f076fb507b37f10b5d50a136e0f72534ec45
michael seaman <mseaman26@gmail.com>
2024-07-17 00:17:39 -0700

    535 MURAL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WHOLE"
-let currentWordleNumber = 534 //<---------Change wordle number here!
+let wordAsString = "MURAL"
+let currentWordleNumber = 535 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
