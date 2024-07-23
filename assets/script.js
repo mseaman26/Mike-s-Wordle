7c210b9dbbb384cdfec0e8be00e48692148033bd
michael seaman <mseaman26@gmail.com>
2024-07-23 01:14:45 -0700

    #538 CARVE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "HOBBY"
-let currentWordleNumber = 537 //<---------Change wordle number here!
+let wordAsString = "CARVE"
+let currentWordleNumber = 538 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
