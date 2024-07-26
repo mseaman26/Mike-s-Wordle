3bf8cecd24ffc20be8e39b6e06f9a2e6f4884b13
michael seaman <mseaman26@gmail.com>
2024-07-26 01:12:05 -0700

    #540 SHRED

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CHEAP"
-let currentWordleNumber = 539 //<---------Change wordle number here!
+let wordAsString = "SHRED"
+let currentWordleNumber = 540 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
