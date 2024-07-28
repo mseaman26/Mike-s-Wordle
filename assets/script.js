fef981c4734b7b6b35ff9615c1e729f92678044a
michael seaman <mseaman26@gmail.com>
2024-07-28 01:43:46 -0700

    #541 MAYBE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SHRED"
-let currentWordleNumber = 540 //<---------Change wordle number here!
+let wordAsString = "MAYBE"
+let currentWordleNumber = 541 //<---------Change wordle number here!
 
 
 if (usedWords.includes(wordAsString)){
