56520d7a4d1c540f641e5b70b3ea06774cf93e77
michael seaman <mseaman26@gmail.com>
2024-08-01 11:27:08 -0700

    #545 PAGAN

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "MOGUL"
-let currentWordleNumber = 544
+let wordAsString = "PAGAN"
+let currentWordleNumber = 545
  //<---------Change wordle number here!
 
 
