2446ad7621988fd5ebc941b822fc6044ca9daf67
michael seaman <mseaman26@gmail.com>
2024-08-11 02:00:33 -0700

    #551 GRAVY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "BREAK"
-let currentWordleNumber = 550
+let wordAsString = "GRAVY"
+let currentWordleNumber = 551
  //<---------Change wordle number here!
 
 
