0ee8722f3b2dba8ea07fcb91925727f65d1b806b
michael seaman <mseaman26@gmail.com>
2024-08-16 01:41:20 -0700

    #555 SWANK

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SWOOP"
-let currentWordleNumber = 554
+let wordAsString = "SWANK"
+let currentWordleNumber = 555
  //<---------Change wordle number here!
 
 
