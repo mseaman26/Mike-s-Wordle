5500d3190b1b7ed4a6e5c25a0423cea23fa0e1d6
michael seaman <mseaman26@gmail.com>
2024-09-01 23:57:46 -0700

    #562 PUREE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PIANO"
-let currentWordleNumber = 561
+let wordAsString = "PUREE"
+let currentWordleNumber = 562
  //<---------Change wordle number here!
 
 
