72762ec3449face179d8b7e8a94a098213f1f467
michael seaman <mseaman26@gmail.com>
2024-09-02 23:12:18 -0700

    #563 EASEL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PUREE"
-let currentWordleNumber = 562
+let wordAsString = "EASEL"
+let currentWordleNumber = 563
  //<---------Change wordle number here!
 
 
