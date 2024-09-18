7d873ca4d6f141b2692987173e4472a3476eb493
michael seaman <mseaman26@gmail.com>
2024-09-18 00:56:34 -0700

    #572 GENIE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FLOOD"
-let currentWordleNumber = 571
+let wordAsString = "GENIE"
+let currentWordleNumber = 572
  //<---------Change wordle number here!
 
 
