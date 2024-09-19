16c6b9c14f9ed412e282694df904de6fb82d7c1e
michael seaman <mseaman26@gmail.com>
2024-09-19 10:59:07 -0700

    #573 MOURN

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "GENIE"
-let currentWordleNumber = 572
+let wordAsString = "MOURN"
+let currentWordleNumber = 573
  //<---------Change wordle number here!
 
 
