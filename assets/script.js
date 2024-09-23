c903a60ec5e2ae1647160204ab8bd3f37adb9f49
michael seaman <mseaman26@gmail.com>
2024-09-23 01:58:26 -0700

    #576 PLUMP

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ADEPT"
-let currentWordleNumber = 575
+let wordAsString = "PLUMP"
+let currentWordleNumber = 576
  //<---------Change wordle number here!
 
 
