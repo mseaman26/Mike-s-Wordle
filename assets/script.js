796c346bcadbf48aa08186bee99c666818358489
michael seaman <mseaman26@gmail.com>
2024-09-25 02:01:59 -0700

    #577 SPARK

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PLUMP"
-let currentWordleNumber = 576
+let wordAsString = "SPARK"
+let currentWordleNumber = 577
  //<---------Change wordle number here!
 
 
