d42192b374f233649b23dc5f8b9b230a8b62df27
michael seaman <mseaman26@gmail.com>
2024-10-06 00:01:19 -0700

    #584 AGISM

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "VULVA"
-let currentWordleNumber = 583
+let wordAsString = "AGISM"
+let currentWordleNumber = 584
  //<---------Change wordle number here!
 
 
