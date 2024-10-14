a1602bd64de739c7d76d3a91fb66250be87bb492
michael seaman <mseaman26@gmail.com>
2024-10-14 00:33:58 -0700

    #589 PSALM

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "BEACH"
-let currentWordleNumber = 588
+let wordAsString = "PSALM"
+let currentWordleNumber = 589
  //<---------Change wordle number here!
 
 
