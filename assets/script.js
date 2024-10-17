308556fce01a829557447f80eb8359b79f143825
michael seaman <mseaman26@gmail.com>
2024-10-16 18:59:24 -0700

    #590 LEASH

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PSALM"
-let currentWordleNumber = 589
+let wordAsString = "LEASH"
+let currentWordleNumber = 590
  //<---------Change wordle number here!
 
 
