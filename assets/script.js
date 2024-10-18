13aa14ab0ab57b8239a5c7d6bcfc2bf19e567060
michael seaman <mseaman26@gmail.com>
2024-10-18 00:02:40 -0700

    #591 SUPER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LEASH"
-let currentWordleNumber = 590
+let wordAsString = "v"
+let currentWordleNumber = 591
  //<---------Change wordle number here!
 
 
