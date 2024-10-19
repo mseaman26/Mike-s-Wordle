7553c23cfc125b956315f7f4b67e6d8cd2fe4976
michael seaman <mseaman26@gmail.com>
2024-10-18 17:01:51 -0700

    #592 SUPER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "v"
-let currentWordleNumber = 591
+let wordAsString = "SUPER"
+let currentWordleNumber = 592
  //<---------Change wordle number here!
 
 
