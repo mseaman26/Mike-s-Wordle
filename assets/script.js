66a6f867c8f0b4956170bdd3d581b87b6d8437d3
michael seaman <mseaman26@gmail.com>
2024-10-21 02:13:17 -0700

    #595 CHALK

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "TOWEL"
-let currentWordleNumber = 594
+let wordAsString = "CHALK"
+let currentWordleNumber = 595
  //<---------Change wordle number here!
 
 
