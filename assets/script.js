30565b75089c934c8e7e4e1cc23fdc5fb8c6ad89
michael seaman <mseaman26@gmail.com>
2024-10-24 01:44:47 -0700

    #597 EAGLE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "REPEL"
-let currentWordleNumber = 596
+let wordAsString = "EAGLE"
+let currentWordleNumber = 597
  //<---------Change wordle number here!
 
 
