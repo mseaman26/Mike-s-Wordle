17ddee7a38d6a9d24b3e33753ddc4cbe72b40249
michael seaman <mseaman26@gmail.com>
2024-10-28 23:00:06 -0700

    #600 ORBIT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "TRUST"
-let currentWordleNumber = 599
+let wordAsString = "ORBIT"
+let currentWordleNumber = 600
  //<---------Change wordle number here!
 
 
