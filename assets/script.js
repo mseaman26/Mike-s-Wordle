6064b432ca2289b4763d4e5f06f9043bdcd18eee
michael seaman <mseaman26@gmail.com>
2024-11-05 14:25:36 -0800

    #604 FATAL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LEERY"
-let currentWordleNumber = 603
+let wordAsString = "FATAL"
+let currentWordleNumber = 604
  //<---------Change wordle number here!
 
 
