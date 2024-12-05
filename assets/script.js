ca861f75199283ea2f18235ac626bf415199632a
michael seaman <mseaman26@gmail.com>
2024-12-04 22:59:04 -0800

    #623 STIFF

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "RHYME"
-let currentWordleNumber = 622
+let wordAsString = "STIFF"
+let currentWordleNumber = 623
  //<---------Change wordle number here!
 
 
