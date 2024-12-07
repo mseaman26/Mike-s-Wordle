45c9764445827e601f2178f34928327f09d1c108
michael seaman <mseaman26@gmail.com>
2024-12-07 01:49:01 -0800

    #624 ELBOW

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "STIFF"
-let currentWordleNumber = 623
+let wordAsString = "ELBOW"
+let currentWordleNumber = 624
  //<---------Change wordle number here!
 
 
