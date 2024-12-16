3de1a360aa98c13d0c67b90bc270dd3b7071559a
michael seaman <mseaman26@gmail.com>
2024-12-16 01:58:40 -0800

    #628 WINCE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PROWL"
-let currentWordleNumber = 627
+let wordAsString = "WINCE"
+let currentWordleNumber = 628
  //<---------Change wordle number here!
 
 
