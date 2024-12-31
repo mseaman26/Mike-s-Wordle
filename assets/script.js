bf12381524a978483c932dace0a1fb80f43befe4
michael seaman <mseaman26@gmail.com>
2024-12-31 11:26:11 -0800

    #636 BIRTH

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SMELL"
-let currentWordleNumber = 635
+let wordAsString = "BIRTH"
+let currentWordleNumber = 636
  //<---------Change wordle number here!
 
 
