9a70bc3709e5e2c904df9c45fe8cf9516f482e02
michael seaman <mseaman26@gmail.com>
2025-01-03 01:59:56 -0800

    #639

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "IMBUE"
-let currentWordleNumber = 638
+let wordAsString = "VOILA"
+let currentWordleNumber = 639
  //<---------Change wordle number here!
 
 
