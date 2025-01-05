fd07f990354468bf6e01a218d91d499f2226ea2b
michael seaman <mseaman26@gmail.com>
2025-01-05 10:49:37 -0800

    #640 ALLOT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "VOILA"
-let currentWordleNumber = 639
+let wordAsString = "ALLOT"
+let currentWordleNumber = 640
  //<---------Change wordle number here!
 
 
