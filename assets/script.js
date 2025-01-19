eedac5cbdae164c0f1c7c7b644cb461122989b4a
michael seaman <mseaman26@gmail.com>
2025-01-18 22:22:07 -0800

    #646 UNBOX

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "HASTE";
-let currentWordleNumber = 645
+let wordAsString = "UNBOX";
+let currentWordleNumber = 646
  //<---------Change wordle number here!
 
 
