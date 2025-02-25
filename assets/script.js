c30a9c3792f5b78cf21897c5a6a80afb7e576021
michael seaman <mseaman26@gmail.com>
2025-02-24 18:34:07 -0800

    #653 FALSE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PROSE";
-let currentWordleNumber = 652
+let wordAsString = "FALSE";
+let currentWordleNumber = 653
  //<---------Change wordle number here!
 
 
