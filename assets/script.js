8f01ca80be494dc9b2b88ed7379bfb89c95033db
michael seaman <mseaman26@gmail.com>
2025-02-24 19:27:19 -0800

    #654 KAZOO

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FALSE";
-let currentWordleNumber = 653
+let wordAsString = "KAZOO";
+let currentWordleNumber = 654
  //<---------Change wordle number here!
 
 
