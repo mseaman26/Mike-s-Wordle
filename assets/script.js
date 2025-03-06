f94591b40226b13712ef0d68713621c4818979c4
michael seaman <mseaman26@gmail.com>
2025-03-06 01:05:54 -0800

    #658 SOBER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SPRAY";
-let currentWordleNumber = 657
+let wordAsString = "SOBER";
+let currentWordleNumber = 658
  //<---------Change wordle number here!
 
 
