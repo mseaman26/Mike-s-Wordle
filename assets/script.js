0e197abdbe92944d541d7938d0d12d384ced07af
michael seaman <mseaman26@gmail.com>
2025-04-08 02:11:16 -0700

    64 NAIVE

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "STYLE";
-let currentWordleNumber = 663
+let wordAsString = "NAIVE";
+let currentWordleNumber = 664
  //<---------Change wordle number here!
 
 
